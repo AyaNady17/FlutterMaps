0eeb9d1952a7a8f9fb34f29022536800b02c0be4
Aya Nady <ayanady22193@gmail.com>
2023-12-02 19:57:15 +0200

    firebase initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,24 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_maps/app_router.dart';
 
-void main() {
-  runApp(const FlutterMaps());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(FlutterMaps(
+    appRouter: AppRouter(),
+  ));
 }
 
 class FlutterMaps extends StatelessWidget {
-  const FlutterMaps({super.key});
-
+  const FlutterMaps({super.key, required this.appRouter});
+  final AppRouter appRouter;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Maps',
-      theme: ThemeData(
-       primarySwatch: Colors.blue
-      ),
-      onGenerateRoute: ,
+      theme: ThemeData(primarySwatch: Colors.blue),
+      onGenerateRoute: appRouter.generateRoute,
     );
   }
 }
-
