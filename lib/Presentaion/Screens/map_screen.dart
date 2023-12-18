90c6eeb278fe8ca3c604a3a3bb6fac531a78d212
Aya Nady <ayanady22193@gmail.com>
2023-12-18 08:09:14 +0200

    build map function done

--- lib/Presentaion/Screens/map_screen.dart
+++ lib/Presentaion/Screens/map_screen.dart
@@ -1,19 +1,54 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_maps/Bussines%20Logic/cubit/phoneAuthCubit/phone_auth_cubit.dart';
+import 'package:flutter_maps/Helpers/location_helper.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class MapScreen extends StatelessWidget {
+class MapScreen extends StatefulWidget {
   MapScreen({super.key});
+
+  @override
+  State<MapScreen> createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
   PhoneAuthCubit phoneAuthCubit = PhoneAuthCubit();
+  static Position? position;
+  static final CameraPosition cameraPosition = CameraPosition(
+      target: LatLng(position!.latitude, position!.longitude), zoom: 17);
+  final Completer<GoogleMapController> _controller = Completer();
+
+  Future<void> getMyCurrentLocation() async {
+    await LocationHelper.determineCurrentLocation();
+    position = await Geolocator.getLastKnownPosition().whenComplete(() {
+      setState(() {});
+    });
+  }
+
+  Widget buildMap() {
+    return GoogleMap(
+      initialCameraPosition: cameraPosition,
+      mapType: MapType.normal,
+      zoomControlsEnabled: false,
+      myLocationButtonEnabled: false,
+      myLocationEnabled: true,
+      onMapCreated: (GoogleMapController controller) {
+        _controller.complete(controller);
+      }, //msh fahma el onmapcreated
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return BlocProvider<PhoneAuthCubit>(
-      create: (context) => phoneAuthCubit,
-      child: ElevatedButton(
-          onPressed: () {
-            phoneAuthCubit.logOut();
-          },
-          child: const Text('logout')),
+    return Stack(
+      children: [
+        position != null
+            ? buildMap()
+            : const Center(child: CircularProgressIndicator()),
+      ],
     );
   }
 }
